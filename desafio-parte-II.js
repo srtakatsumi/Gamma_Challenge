dc3774ae6a6c4658e8501f88a36712f162a0c5df
Srta Katsumi <73000702+srtakatsumi@users.noreply.github.com>
2021-05-13 23:02:57 -0300

    Update desafio-parte-II.js

--- desafio-parte-II.js
+++ desafio-parte-II.js
@@ -1,1 +1,149 @@
-
+var listProducts = [
+    {
+    "idProduto": 471185,
+    "descricao": "Suporte para monitor quadrado AC125 UN0001 Multilaser",
+    "preco": 64.17,
+    "qtdEstoque": 149,
+    "disponivel": "sim",
+    "emDestaque": "não",
+    "departamento": 
+        {
+        "idDepto": 1,
+        "nomeDepto": "Acessórios para computadores"
+         }
+   } , {
+    "idProduto": 471184,
+    "descricao": "Suporte para monitor triangular AC124 UN0001 Multilaser",
+    "preco": 54.69,
+    "qtdEstoque": 7,
+    "disponivel": "sim",
+    "emDestaque": "não",
+    "departamento": 
+        {
+        "idDepto": 1,
+        "nomeDepto": "Acessórios para computadores"
+        }
+    },{
+    "idProduto": 435970,
+    "descricao": "Apoio ergonômico para pés Waleu",
+    "preco": 91.98,
+    "qtdEstoque": 102 ,
+    "disponivel": "sim",
+    "emDestaque": "sim",
+    "departamento": 
+        {
+        "idDepto": 1,
+        "nomeDepto": "Acessórios para computadores"
+        }
+  } , {
+    "idProduto": 380385,
+    "descricao": "Suporte para cpu com roda",
+    "preco": 62.48,
+    "qtdEstoque": 33,
+    "disponivel": "sim",
+    "emDestaque": "sim",
+    "departamento": 
+        {
+        "idDepto": 1,
+        "nomeDepto": "Acessórios para computadores"
+        } 
+  } , {
+    "idProduto":402265 ,
+    "descricao": "Lápis preto com borracha redondo tabuada PO0072 Leo&Leo",
+    "preco": 26.53,
+    "qtdEstoque": 45,
+    "disponivel": "sim",
+    "emDestaque": "sim",
+    "departamento": 
+        {
+        "idDepto": 2,
+        "nomeDepto": "Lápis de escrever"
+        }
+  } , {
+    "idProduto": 478585,
+    "descricao": "Lápis preto c/borracha redondo pastel PO0072 Leo&Leo",
+    "preco": 40.17,
+    "qtdEstoque": 0,
+    "disponivel": "não",
+    "emDestaque": "não",
+    "departamento": 
+        {
+        "idDepto": 2,
+        "nomeDepto": "Lápis de escrever"
+        }
+  } , {
+    "idProduto": 473747,
+    "descricao": "Lápis Preto Grafite Nº 2 Faber-Castell - Cartela com 04 Lápis",
+    "preco": 4.52,
+    "qtdEstoque": 255,
+    "disponivel": "sim",
+    "emDestaque": "sim",
+    "departamento": 
+       {
+        "idDepto": 2,
+        "nomeDepto": "Lápis de escrever"
+        }
+  } , {
+    "idProduto": 473141,
+    "descricao": "Brocal Escolar 3G Sortido Lantecor - Pacote com 12 Unidades",
+    "preco": 5.01,
+    "qtdEstoque": 33,
+    "disponivel": "sim",
+    "emDestaque": "não",
+    "departamento": 
+        {
+        "idDepto": 3,
+        "nomeDepto": "Artesanato"
+        }
+  } , {
+    "idProduto": 472627,
+    "descricao": "Tesoura uso geral 21cm BL0001 Jocar",
+    "preco": 6.66,
+    "qtdEstoque": 66,
+    "disponivel": "sim",
+    "emDestaque": "sim",
+    "departamento": 
+        {
+        "idDepto": 3,
+        "nomeDepto": "Artesanato"
+        }
+  } , {
+    "idProduto": 473129,
+    "descricao": "Lantejoula 06MM 3G Sortido Lantecor - Pacote com 12 Unidades",
+    "preco": 5.01,
+    "qtdEstoque": 844,
+    "disponivel": "sim",
+    "emDestaque": "sim",
+    "departamento": 
+        {
+        "idDepto": 3,
+        "nomeDepto": "Artesanato"
+        }
+  } , {
+  
+    "idProduto": 481432,
+    "descricao": "Agenda Diária Espiral O Pequeno Príncipe Azul Escuro 2021 Jandaia",
+    "preco": 7.37,
+    "qtdEstoque": 17 ,
+    "disponivel": "sim",
+    "emDestaque": "sim",
+    "departamento": 
+        {
+        "idDepto": 4,
+        "nomeDepto": "Agenda" 
+        }
+  } , {
+  
+    "idProduto": 481435,
+    "descricao": "Agenda Diária Espiral Eco Línea Rosa 2021 Jandaia",
+    "preco": 7.37,
+    "qtdEstoque": 14,
+    "disponivel": "sim",
+    "emDestaque": "sim",
+    "departamento": 
+        {
+        "idDepto": 4,
+        "nomeDepto": "Agenda" 
+        }
+      }
+    ]
